d6d1e7a1dabb0c99ad7cfdda6994857803b63217
Alexander Pape <a.pape@paneon.de>
2021-12-03 12:37:18 +0100

    Add sentry for frontend error tracking

---
+++ assets/plugins/sentry.ts
@@ -0,0 +1,19 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+declare const SENTRY_FRONTEND: string;
+
+if (typeof SENTRY_FRONTEND === 'undefined') {
+    console.log('Sentry not defined.');
+} else {
+    Sentry.init({
+        // Placeholder for Webpack
+        dsn: SENTRY_FRONTEND,
+        integrations: [new Integrations.BrowserTracing()],
+
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    });
+}
