e4292cd752e1d6afac255689c5469f03e43c571a
Alexander Pape <a.pape@paneon.de>
2021-12-03 15:55:13 +0100

    Add Sentry for backend error tracking

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Rollbar\Symfony\RollbarBundle\RollbarBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
